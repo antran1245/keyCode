957dcd884eee289b4b5295ad4243a4cef81d489d
antran1245 <antran1245@gmail.com>
2022-09-19 20:43:36 -0700

    ascii code, readme, need to worry about shift,etc

--- README.md
+++ README.md
@@ -2,10 +2,26 @@
 
 When a key is pressed, the keyCode will be displayed.
 
+## Run
+
+Run in an virtual environment using **`pipenv shell`**.
+
+If you setup a `.flaskenv`, run the command **`flask run`**
+
+or
+
+**`python server.js`**
+
 ## Tech
 
 ### Languages
+- JavaScript
+    - Focus on the screen when clicking anywhere on the screen.
+    - Detect key press.
 - HTML
+    - Front End design
 - CSS
+    - Style
 - Python
+    - Server
 
--- flask_app/static/index.js
+++ flask_app/static/index.js
@@ -1,5 +1,7 @@
 const inputField = document.querySelector('#keyButton')
-const charField = document.querySelector('#charCode')
+const ascii = document.querySelector('#ascii')
+const keyField = document.querySelector('#key')
+const charField = document.querySelector('#keyCode')
 
 function focusInput() {
     const keyInput = inputField.focus()
@@ -7,5 +9,6 @@ function focusInput() {
 
 window.addEventListener('keydown', function(e) {
     inputField.value = e.key
-    charField.innerHTML = e.which
+    ascii.innerHTML = e.key.charCodeAt(0)
+    charField.innerHTML = e.code
 }, false)
