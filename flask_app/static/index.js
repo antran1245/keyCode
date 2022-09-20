b3b9afd423837e47e384c1414231f0c7eb9255be
antran1245 <antran1245@gmail.com>
2022-09-20 13:55:10 -0700

    showing that shift was pressed down with another

--- flask_app/static/index.js
+++ flask_app/static/index.js
@@ -1,14 +1,50 @@
 const inputField = document.querySelector('#keyButton')
 const ascii = document.querySelector('#ascii')
-const keyField = document.querySelector('#key')
+const codeField = document.querySelector('#code')
 const charField = document.querySelector('#keyCode')
+let shift = ""
 
 function focusInput() {
     const keyInput = inputField.focus()
 }
 
 window.addEventListener('keydown', function(e) {
+    //key pressed
     inputField.value = e.key
-    ascii.innerHTML = e.key.charCodeAt(0)
-    charField.innerHTML = e.code
+    
+    //determine the Ascii code of key pressed
+    let charCode = e.key.charCodeAt(0)
+    if (e.key === "Shift" || e.key === 'Control' || e.key === 'Alt' || e.key === 'CapsLock') {
+        charCode = 0
+    }
+    if (e.key === 'Enter') {
+        charCode = 13
+    }
+    if (e.key === 'Backspace') {
+        charCode = 8
+    }
+    if (e.key === 'Space') {
+        charCode = 32
+    }
+    if (e.key === 'Escape') {
+        charCode = 27
+    }
+    ascii.innerHTML = charCode
+
+    if (e.key === "Shift") {
+        shift = "Shift + "
+    }
+
+    //keyCode of key pressed
+    charField.innerHTML = shift+e.keyCode
+
+    //code of key  pressed
+    codeField.innerHTML = e.code
+    console.log(e.key)
 }, false)
+
+window.addEventListener('keyup', (e) => {
+    if(e.key === "Shift") {
+        shift = ""
+    }
+})
